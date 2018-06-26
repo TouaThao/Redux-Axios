43d10051833846a47b93364620f1323118513761
Christopher Black <chris@primeacademy.io>
2018-06-26 14:39:23 -0500

    Added color array, reducers and map redux to props.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 
+// Allowing us to access reduxState on props
+const mapReduxStateToProps = (reduxStore) => ({
+  reduxStore
+})
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      color: ''
+    }
+  }
+
+  handleColorChange = (event) => {
+    this.setState({
+      color: event.target.value
+    })
+  }
+
+  sendColorToRedux = () => {
+    // Send the color to redux with an action type of ADD_COLOR
+    const action = {type: 'ADD_COLOR', payload: this.state.color};
+    this.props.dispatch(action);
+  }
+
+  deleteAllColors = () => {
+    const action = {type: 'DELETE_COLORS'};
+    this.props.dispatch(action);
+  }
+
   render() {
+    console.log('Rendering App.js');
     return (
       <div className="App">
-        <button onClick={() => this.props.dispatch({type: 'BUTTON_ONE'})}>Button One</button>
+        <pre>{JSON.stringify(this.props.reduxStore)}</pre>
+        {/* The value of the reducer will be what state is */}
+        <div>{this.props.reduxStore.counterReducer}</div>
+        {/* dispatch takes in an action */}
+        <button onClick={() => this.props.dispatch({type: 'ADD'})}>Add</button>
+        {/* Subtract button */}
+        <button onClick={() => this.props.dispatch({type: 'SUBTRACT'})}>Subtract</button>
+
+        <h3>Enter Color Here:</h3>
+        <input onChange={this.handleColorChange} value={this.state.color} />
+        <button onClick={this.sendColorToRedux}>Submit</button>
+        <button onClick={this.deleteAllColors}>Delete ALL Colors</button>
+        
       </div>
     );
   }
 }
 
-export default connect()(App); // Currying, a function that returns a function
+// connect() allows us to dispatch
+// connect(mapReduxStateToProps) to access information
+export default connect(mapReduxStateToProps)(App); // Currying, a function that returns a function
--- src/index.js
+++ src/index.js
@@ -3,26 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger'; // Makes development easier
 
-const firstReducer = (state, action) => {
-    console.log(`Hey!!! I'm a reducer.`, action);
-    return {};
+// This will increase or decrease a number, starts at 0 (default)
+// Whatever we return, is passed in as 'state' on the next function call
+const counterReducer = (state = 0, action) => {
+    // Filter down to specific action types
+    if(action.type === 'ADD') {
+        console.log(`Hey!!! I'm a reducer.`, action);
+        // return the new state
+        return state + 1;
+        // nothing after a return is called
+    }
+    if (action.type === 'SUBTRACT' && state > 0) {
+        return state - 1;
+    }
+    return state; // return the current state if no changes were made
 }
 
-const secondReducer = (state, action) => {
-    console.log(`Second reducer.`);
-    return {};
+const secondReducer = (state = [], action) => {
+    if(action.type === 'ADD_COLOR') {
+        // Adding our new color to the array
+        return [...state, action.payload];
+    } else if(action.type === 'DELETE_COLORS') {
+        return [];
+    }
+    return state;
 }
 
 const storeInstance = createStore(
     // This is a reducer. It's called when the page loads or
     // dispatch is called.
     combineReducers({
-        firstReducer,
+        counterReducer,
         secondReducer
-    })
+    }),
+    applyMiddleware(logger)
 );
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
